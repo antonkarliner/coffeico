b58f10f07a54cbcd7eaf311e18ba51a8dfad210a
Anton Karliner <antonkarliner@gmail.com>
2025-05-09 13:37:25 +0200

    feat: add icons for new methods

--- flutter/lib/coffeico.dart
+++ flutter/lib/coffeico.dart
@@ -6,32 +6,129 @@ class Coffeico {
   static const _fontFamily = 'Coffeico';
   static const _fontPackage = 'coffeico';
 
-  static const IconData aeropress =
-      IconData(0xf101, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData chemex =
-      IconData(0xf102, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData clever_dripper =
-      IconData(0xf103, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData french_press =
-      IconData(0xf104, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData hario_v60 =
-      IconData(0xf105, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData kalita_wave =
-      IconData(0xf106, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData origami =
-      IconData(0xf107, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData wilfa_svart =
-      IconData(0xf108, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData bean =
-      IconData(0xf109, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData coffee_maker =
-      IconData(0xf10a, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData portafilter =
-      IconData(0xf10b, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData bag =
-      IconData(0xf10c, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData bag_with_bean =
-      IconData(0xf10d, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData hario_switch =
-      IconData(0xf10e, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData aeropress = IconData(
+    0xf101,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData chemex = IconData(
+    0xf102,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData clever_dripper = IconData(
+    0xf103,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData french_press = IconData(
+    0xf104,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData hario_v60 = IconData(
+    0xf105,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData kalita_wave = IconData(
+    0xf106,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData origami = IconData(
+    0xf107,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData wilfa_svart = IconData(
+    0xf108,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData bean = IconData(
+    0xf109,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData coffee_maker = IconData(
+    0xf10a,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData portafilter = IconData(
+    0xf10b,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData bag = IconData(
+    0xf10c,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData bag_with_bean = IconData(
+    0xf10d,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData hario_switch = IconData(
+    0xf10e,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData melita_cone = IconData(
+    0xf10f,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData bee_house_dripper = IconData(
+    0xf110,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData phin_filter = IconData(
+    0xf111,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData neapolitan_flip_pot = IconData(
+    0xf112,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData moka_pot = IconData(
+    0xf113,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData siphon_brewer = IconData(
+    0xf114,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData cezve = IconData(
+    0xf115,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData tricolate = IconData(
+    0xf116,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData pulsar = IconData(
+    0xf117,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData ufo_dripper = IconData(
+    0xf118,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
+  static const IconData orea_brewer = IconData(
+    0xf119,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
+  );
 }
